198f352ee458dc9907d6efb74aa24a1302065f42
Adrian <adiszymczyk@gmail.com>
2024-04-27 14:16:07 +0200

    Initial Commit

---
+++ lab2AS/lab2ASApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2ASApp.swift
+//  lab2AS
+//
+//  Created by Adrian Szymczyk on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2ASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
